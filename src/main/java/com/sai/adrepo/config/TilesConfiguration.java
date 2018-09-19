e62b0cddc2ee1165c37213d95b50933e86ad3529
DESKTOP-PI0NLPF\nguye <nguyenbatrinh0211@gmail.com>
2018-09-20 00:20:14 +0700

    temp - tiles template layout

---
+++ src/main/java/com/sai/adrepo/config/TilesConfiguration.java
@@ -0,0 +1,26 @@
+package com.sai.adrepo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesView;
+import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
+
+@Configuration
+public class TilesConfiguration implements WebMvcConfigurer {
+    @Bean
+    public TilesConfigurer tilesConfigurer() {
+        TilesConfigurer tilesConfigurer = new TilesConfigurer();
+        tilesConfigurer.setDefinitions(new String[]{"WEB-INF/tiles.xml"});
+        tilesConfigurer.setCheckRefresh(true);
+        return tilesConfigurer;
+    }
+
+    @Bean
+    public TilesViewResolver tilesViewResolver() {
+        final TilesViewResolver resolver = new TilesViewResolver();
+        resolver.setViewClass(TilesView.class);
+        return resolver;
+    }
+}
