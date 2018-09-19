987372bb20397c96daa94cb6fb8a1d6c3525523a
DESKTOP-PI0NLPF\nguye <nguyenbatrinh0211@gmail.com>
2018-09-20 00:18:27 +0700

    build spring boot

---
+++ src/main/java/com/sai/adrepo/AdrepoApplication.java
@@ -0,0 +1,11 @@
+package com.sai.adrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdrepoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdrepoApplication.class, args);
+    }
+}
